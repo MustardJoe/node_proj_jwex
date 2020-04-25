1dab35f26dc0b4795b043fbb904a0d78cab849b0
Jonathan Forney <jonathan.forney@gmail.com>
2020-04-24 18:35:21 -0700

    moved the  cities thing and got a hello world http server up

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-const cities = require('cities');
+const port = 3000,
+  http = require('http'),
+  httpStatus = require('http-status-codes'),
+  app = http.createServer((req, res) => {
+    console.log('received an incoming request');
+    res.writeHead(httpStatus.OK, {
+      'Content-Type': 'text/html'
+    });
+    let responseMessage = `<h1>Hello dumb world, (from smart world)</h1>
+    <p>all your butts are belong to us.</p>
+    `;
+    res.write(responseMessage);
+    res.end();
+    console.log(`Sent response message : ${responseMessage}`);
+  })
 
-let myLocation = cities.zip_lookup('97215');
-
-console.log(myLocation);
+  app.listen(port);
+  console.log(`The server has started and is listening on port ${port}. Welcome to SkyNet.`)
