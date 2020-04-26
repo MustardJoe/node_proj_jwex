f9997fc6d919de6fae0c9e0a061dfc21d6403adc
Jonathan Forney <jonathan.forney@gmail.com>
2020-04-25 23:17:50 -0700

    🤢🤮added linter and linted main.js

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-const port = 3000,
-  http = require('http'),
-  httpStatus = require('http-status-codes'),
-  app = http.createServer((req, res) => {
-    console.log('received an incoming request');
-    res.writeHead(httpStatus.OK, {
-      'Content-Type': 'text/html'
-    });
-    let responseMessage = `<h1>Hello dumb world, (from smart world)</h1>
-    <p>all your butts are belong to us.</p>
-    `;
-    res.write(responseMessage);
-    res.end();
-    console.log(`Sent response message : ${responseMessage}`);
-  })
+const http = require('http');
+const httpStatus = require('http-status-codes');
 
-  app.listen(port);
-  console.log(`The server has started and is listening on port ${port}. Welcome to SkyNet.`)
+const port = 3000;
+const app = http.createServer((req, res) => {
+  /* eslint-disable-next-line no-console */
+  console.log('received an incoming request');
+  res.writeHead(httpStatus.OK, {
+    'Content-Type': 'text/html'
+  });
+  let responseMessage = `<h1>Hello dumb world, (from smart world)</h1>
+  <p>all your butts are belong to us.</p>
+  `;
+  res.write(responseMessage);
+  res.end();
+  /* eslint-disable-next-line no-console */
+  console.log(`Sent response message : ${responseMessage}`);
+});
+
+app.listen(port);
+console.log(`The server has started and is listening on port ${port}. Welcome to SkyNet.`)
