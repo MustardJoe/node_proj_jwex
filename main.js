2d2780d0de8f4dea6d2949cd66f86ebe38e3fb8b
Jonathan Forney <jonathan.forney@gmail.com>
2020-04-27 18:11:39 -0700

    some basic routing set up

--- main.js
+++ main.js
@@ -2,20 +2,43 @@ const http = require('http');
 const httpStatus = require('http-status-codes');
 
 const port = 3000;
-const app = http.createServer((req, res) => {
+const app = http.createServer();
+
+const routesMap = {
+  '/info': '<h1>Welcome to info</h1>',
+  '/contact': '<h1>Unwelcome to contact me</h1>',
+  '/about': '<h1>Now you learn AbOUT me (i aint commie-nadian</h1>',
+  '/hell': '<h1>HELL!!!! YOU ARE DAMNED I GUESS FOR COMING TO HERE</h1>',
+  '/error': '<h1>An Error Has Occured</h1>',
+  '/': '<h1>we will see if this works: its home!!!</h1>',
+};
+
+app.on('request', (req, res) => {
   /* eslint-disable-next-line no-console */
   console.log('received an incoming request');
+  /* eslint-disable-next-line no-console */
+  console.log('method: ', req.method, 'url: ', req.url, 'headers', req.headers);
+  
   res.writeHead(httpStatus.OK, {
     'Content-Type': 'text/html'
   });
-  let responseMessage = `<h1>Hello dumb world, (from smart world)</h1>
-  <p>all your butts are belong to us.</p>
-  `;
+
+  let responseMessage = '';
+  if(routesMap[req.url]) {
+    responseMessage = routesMap[req.url];
+  }
+  else {
+    responseMessage = routesMap['/error'];
+  }
+  
+  
   res.write(responseMessage);
+
   res.end();
   /* eslint-disable-next-line no-console */
   console.log(`Sent response message : ${responseMessage}`);
 });
 
 app.listen(port);
-console.log(`The server has started and is listening on port ${port}. Welcome to SkyNet.`)
+/* eslint-disable-next-line no-console */
+console.log(`The server has started and is listening on port ${port}. Welcome to SkyNet.`);
