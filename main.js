1f1d7aeed018f5228912c74625300881b28d7fe9
Jonathan Forney <jonathan.forney@gmail.com>
2020-04-27 22:05:46 -0700

    server rendering html both from module and from single lines

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 const http = require('http');
 const httpStatus = require('http-status-codes');
+const fs = require('fs');
 
-const port = 3000;
 const app = http.createServer();
 
-const routesMap = {
+const port = 3000;
+
+const msgsMap = {
   '/info': '<h1>Welcome to info</h1>',
   '/contact': '<h1>Unwelcome to contact me</h1>',
   '/about': '<h1>Now you learn AbOUT me (i aint commie-nadian</h1>',
   '/hell': '<h1>HELL!!!! YOU ARE DAMNED I GUESS FOR COMING TO HERE</h1>',
   '/error': '<h1>An Error Has Occured</h1>',
-  '/': '<h1>we will see if this works: its home!!!</h1>',
+};
+
+const routesMap = {
+  '/': './views/index.html'
 };
 
 app.on('request', (req, res) => {
   /* eslint-disable-next-line no-console */
   console.log('received an incoming request');
   /* eslint-disable-next-line no-console */
-  console.log('method: ', req.method, 'url: ', req.url, 'headers', req.headers);
+  console.log('method: ', req.method, 'url: ', req.url);
   
   res.writeHead(httpStatus.OK, {
     'Content-Type': 'text/html'
@@ -25,16 +30,33 @@ app.on('request', (req, res) => {
 
   let responseMessage = '';
   if(routesMap[req.url]) {
-    responseMessage = routesMap[req.url];
+    fs.readFile(routesMap[req.url], (error, data) => {
+      res.write(data);
+      res.end();
+    });
   }
   else {
-    responseMessage = routesMap['/error'];
+    if(msgsMap[req.url]) {
+      responseMessage = msgsMap[req.url];
+      
+    }
+    else {
+      responseMessage = msgsMap['/error'];
+    }
+    responseMessage += `
+            available routes:
+            '/info'
+            '/contact'
+            '/about'
+            '/hell'
+            '/error'
+            '/'
+    `;
+
+    res.write(responseMessage);
+    res.end();
   }
   
-  
-  res.write(responseMessage);
-
-  res.end();
   /* eslint-disable-next-line no-console */
   console.log(`Sent response message : ${responseMessage}`);
 });
