42f41e907c026d493928f00c997fd3af3b5cc45e
Jonathan Forney <jonathan.forney@gmail.com>
2020-05-02 01:22:09 -0700

    up through 6.3 in wexler

--- main.js
+++ main.js
@@ -2,65 +2,97 @@ const http = require('http');
 const httpStatus = require('http-status-codes');
 const fs = require('fs');
 
-const app = http.createServer();
+// const app = http.createServer();
 
 const port = 3000;
 
-const msgsMap = {
-  '/info': '<h1>Welcome to info</h1>',
-  '/contact': '<h1>Unwelcome to contact me</h1>',
-  '/about': '<h1>Now you learn AbOUT me (i aint commie-nadian</h1>',
-  '/hell': '<h1>HELL!!!! YOU ARE DAMNED I GUESS FOR COMING TO HERE</h1>',
-  '/error': '<h1>An Error Has Occured</h1>',
-};
+// const msgsMap = {
+//   '/info': '<h1>Welcome to info</h1>',
+//   '/contact': '<h1>Unwelcome to contact me</h1>',
+//   '/about': '<h1>Now you learn AbOUT me (i aint commie-nadian</h1>',
+//   '/hell': '<h1>HELL!!!! YOU ARE DAMNED I GUESS FOR COMING TO HERE</h1>',
+//   '/error': '<h1>An Error Has Occured</h1>',
+// };
 
-const routesMap = {
-  '/': './views/index.html'
-};
+// const routesMap = {
+//   '/': './views/index.html'
+// };
 
-app.on('request', (req, res) => {
-  /* eslint-disable-next-line no-console */
-  console.log('received an incoming request');
-  /* eslint-disable-next-line no-console */
-  console.log('method: ', req.method, 'url: ', req.url);
+// app.on('request', (req, res) => {
+//   /* eslint-disable-next-line no-console */
+//   console.log('received an incoming request');
+//   /* eslint-disable-next-line no-console */
+//   console.log('method: ', req.method, 'url: ', req.url);
   
-  res.writeHead(httpStatus.OK, {
-    'Content-Type': 'text/html'
-  });
+//   res.writeHead(httpStatus.OK, {
+//     'Content-Type': 'text/html'
+//   });
 
-  let responseMessage = '';
-  if(routesMap[req.url]) {
-    fs.readFile(routesMap[req.url], (error, data) => {
-      res.write(data);
+//   let responseMessage = '';
+//   if(routesMap[req.url]) {
+//     fs.readFile(routesMap[req.url], (error, data) => {
+//       res.write(data);
+//       res.end();
+//     });
+//   }
+//   else {
+//     if(msgsMap[req.url]) {
+//       responseMessage = msgsMap[req.url];
+      
+//     }
+//     else {
+//       responseMessage = msgsMap['/error'];
+//     }
+//     responseMessage += `
+//             available routes:
+//             '/info'
+//             '/contact'
+//             '/about'
+//             '/hell'
+//             '/error'
+//             '/'
+//     `;
+
+//     res.write(responseMessage);
+//     res.end();
+//   }
+  
+//   /* eslint-disable-next-line no-console */
+//   console.log(`Sent response message : ${responseMessage}`);
+// });
+// app.listen(port);
+
+const routeMap = {
+  '/': './views/index.html',
+};
+
+const getViewUrl = url => {
+  console.log(`views${url}.html`);
+  return `views${url}.html`;
+};
+
+http
+  .createServer((req, res) => {
+    let url = getViewUrl(req.url);
+    console.log(url);
+     
+    fs.readFile(url, (error, data) => {
+      if(error) {
+        res.writeHead(httpStatus.NOT_FOUND);
+        res.write('<h1>Sorry looser, no data for you</h1>');
+      }
+      else {
+        res.writeHead(httpStatus.OK, {
+          'Content-Type': 'text/html'
+        });
+        res.write(data);
+      }
       res.end();
     });
-  }
-  else {
-    if(msgsMap[req.url]) {
-      responseMessage = msgsMap[req.url];
-      
-    }
-    else {
-      responseMessage = msgsMap['/error'];
-    }
-    responseMessage += `
-            available routes:
-            '/info'
-            '/contact'
-            '/about'
-            '/hell'
-            '/error'
-            '/'
-    `;
+    
+  })
+  .listen(port);
 
-    res.write(responseMessage);
-    res.end();
-  }
-  
-  /* eslint-disable-next-line no-console */
-  console.log(`Sent response message : ${responseMessage}`);
-});
 
-app.listen(port);
 /* eslint-disable-next-line no-console */
 console.log(`The server has started and is listening on port ${port}. Welcome to SkyNet.`);
