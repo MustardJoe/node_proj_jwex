5becd3818256d4cca5c5087bcb693f229b3b76a1
Jonathan Forney <jonathan.forney@gmail.com>
2020-05-05 15:59:32 -0700

    created module to handle header content infor

---
+++ contentTypes.js
@@ -0,0 +1,20 @@
+module.exports = {
+  html: {
+    'Content-Type': 'text/html'
+  },
+  text: {
+    'Content-Type': 'text/plain'
+  },
+  js: {
+    'Constent-Type': 'text/js'
+  },
+  jpg: {
+    'Content-Type': 'image/jpg'
+  },
+  png: {
+    'Content-Type': 'image/png'
+  },
+  css: {
+    'Content-Type': 'text/css'
+  }
+};
