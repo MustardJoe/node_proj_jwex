50a8b18f2238d5d0df6df73b274a300210a58ff0
Jonathan Forney <jonathan.forney@gmail.com>
2020-05-04 20:45:44 -0700

    re-wrote router

--- routes.js
+++ routes.js
@@ -1,80 +1,38 @@
-// const httpStatus = require('http-status-codes');
-// const htmlContentType = {
-//   'Content-Type': 'text/html'
-// };
-
-
-// const routes = {
-//   'GET': {
-//     '/info': (req, res) => {
-//       res.writeHead(httpStatus.OK, {
-//         'Content-Type': 'text/plain'
-//       });
-//       res.end('Welcome to the INFO page');
-//     }
-//   },
-//   'POST': {}
-// };
-
-// exports.handle = (req, res) => {
-//   try {
-//     console.log('in routes', req.url, req.method, routes[req.method][req.url]);
-//     if(routes[req.method][req.url]) {
-//       routes[req.methos][req.url](req, res);
-//     }
-//     else {
-//       res.writeHead(httpStatus.NOT_FOUND, htmlContentType);
-//       res.end('<h1>404 IDIOT!!! STUPID LOOSER THAT FILE DOESN'T EXIST</h1>');
-//     }
-//   }
-//   catch(ex) {
-//     /* eslint-disable-next-line no-console */
-//     console.log('ERROR: ', ex);
-//   }
-// };
-
-// exports.get = (url, action) => {
-//   routes['GET'][url] = action;
-// };
-
-// exports.post = (url, action) => {
-//   routes['POST'][url] = action;
-// };
-
-const httpStatus = require('http-status-codes'),
-  htmlContentType = {
-    'Content-Type': 'text/html'
+const httpStatus = require('http-status-codes');
+const htmlContentType = {
+  'Content-Type': 'text/html'
+};
+const routes = {
+  'GET': {
+    '/info': (req, res) => {
+      res.writeHead(httpStatus.OK, {
+        'Content-Type': 'text/plain'
+      });
+      res.end('<h1>Welcome to the INFO Page silly mortal</h1>');
+    }
   },
-  routes = {
-    'GET': {
-      '/info': (req, res) => {
-        res.writeHead(httpStatus.OK, {
-          'Content-Type': 'text/plain'
-        });
-        res.end('Welcome to the Info Page!');
-      }
-    },
-    'POST': {}
-  };
+  'POST': {}
+};
 
 exports.handle = (req, res) => {
   try {
     if(routes[req.method][req.url]) {
       routes[req.method][req.url](req, res);
-    } else {
-      res.writeHead(httpStatus.NOT_FOUND, htmlContentType);
-      res.end('<h1>No such file exists</h1>');
     }
-  } catch(ex) {
-    /* eslint-disable-next-line no-console */
-    console.log('error: ' + ex);
+    else {
+      res.writeHead(httpStatus.NOT_FOUND, htmlContentType);
+      res.end('<h1>404 Page stupid doo doo dumb</h1>')
+    }
+  }
+  catch(problem) {
+    console.log('Error: ', problem);
   }
 };
 
 exports.get = (url, action) => {
   routes['GET'][url] = action;
 };
+
 exports.post = (url, action) => {
   routes['POST'][url] = action;
 };
-
